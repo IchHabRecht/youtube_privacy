e644371548dacd1abce55edc1b0d24161f25ac16
Nicole Cordes <typo3@cordes.co>
2016-12-07 23:35:57 +0100

    [FEATURE] Add TypoScript to add lib.fluidContent.partialRootPaths

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('youtube', 'Configuration/TypoScript/', 'Render youtube videos with nocookie');
