53aea6409d28d68408401a55b0d8da8865c288ae
Nicole Cordes <typo3@cordes.co>
2016-12-07 23:33:58 +0100

    [TASK] Add basic extension information

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "youtube".
+ *
+ * Auto generated 07-12-2016 23:33
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Youtube',
+  'description' => 'Render youtube videos with nocookie domain',
+  'author' => 'Nicole Cordes',
+  'author_email' => 'typo3@cordes.co',
+  'author_company' => 'IchHabRecht',
+  'category' => 'fe',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-8.5.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'state' => 'stable',
+  'version' => '1.0.0',
+  'clearCacheOnLoad' => 0,
+  'createDirs' => '',
+  'uploadfolder' => 0,
+  '_md5_values_when_last_written' => 'a:1:{s:12:"ext_icon.png";s:4:"66e8";}',
+);
+
